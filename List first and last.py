4789d45dc71c4b6e381ca551550cb7ef42d0e432
Mina Basem <42482261+MinaBasem@users.noreply.github.com>
2018-08-19 02:47:59 +0300

    Add files via upload

---
+++ List first and last.py
@@ -0,0 +1,6 @@
+a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]    #list is called a
+
+def listFunction(list):
+    print(list[0], " and ", list[len(list)-1])
+
+listFunction(a)    # i called the list "a" in the function
