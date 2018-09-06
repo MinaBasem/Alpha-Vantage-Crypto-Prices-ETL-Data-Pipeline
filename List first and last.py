4454fc649c07108b5a104b1e563228dfeffe5efa
Mina Basem <42482261+MinaBasem@users.noreply.github.com>
2018-09-06 21:57:11 +0300

    Update List first and last.py

--- List first and last.py
+++ List first and last.py
@@ -1,6 +1,9 @@
+import time
+
 a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]    #list is called a
 
 def listFunction(list):
     print(list[0], " and ", list[len(list)-1])
 
 listFunction(a)    # i called the list "a" in the function
+time.sleep(5)
